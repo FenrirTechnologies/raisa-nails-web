090f132d4b0f6593d560d44a6be777f60269a060
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2018-01-11 23:40:58 -0500

    setup base projects

---
+++ src/RaisaNails.Users/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace RaisaNails.Users
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
